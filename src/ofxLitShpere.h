f6a8ddded9750e7c83737f11e026097ac1b0a248
Alexey Roudenko <ar@radugadesign.com>
2014-01-05 16:06:57 +0700

    import

---
+++ src/ofxLitShpere.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofxLitSphere {
+    
+public:
+    
+    ofShader    shader;
+    
+    ofImage texture;
+    ofDirectory dir;
+    int current = 0;
+    
+    //--------------------------------------------------------------
+    void setup(){
+        ofEnableNormalizedTexCoords();
+        ofDisableNormalizedTexCoords();
+        ofEnableAlphaBlending();
+            
+        shader.load("shaders/litsphere/vert.glsl", "shaders/litsphere/frag.glsl");
+        loadNext();
+        loadPrevious();
+    }
+    
+    void loadNext() {
+        string directory = "MatCapZBrush/Lib/";
+        dir.listDir(directory);
+        
+        current++;
+        if (dir.size() <= current) current = 0;
+        
+        string fileName = dir.getPath(current);
+        ofLogVerbose("ofxLitSphere", "fileName " + fileName);
+        texture.loadImage(fileName);
+    }
+    
+    void loadPrevious() {
+        string directory = "MatCapZBrush/Lib/";
+        dir.listDir(directory);
+        
+        current--;
+        if (current < 0) current = dir.size() - 1;
+        
+        string fileName = dir.getPath(current);
+        ofLogVerbose("ofxLitSphere", "fileName " + fileName);
+        texture.loadImage(fileName);
+
+    }
+    
+    //--------------------------------------------------------------
+    void update(){}
+    
+    void begin() {
+        ofEnableNormalizedTexCoords();
+        ofEnableTextureEdgeHack();
+        
+        shader.begin();
+        shader.setUniformTexture("litsphereTexture", texture, 1);
+    }
+    
+    void end() {
+        shader.end();
+    }
+    
+    void reload() {
+        shader.load("shaders/litsphere/vert.glsl", "shaders/litsphere/frag.glsl");
+    }
+};
